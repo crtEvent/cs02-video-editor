ed28ff6610d261ea4a7280b4b4b1a58dbc823254
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-13 06:34:28 +0900

    test: 비디오 리스트 get() 메서드 test 추가

--- src/test/java/cs02/model/video/VideoTest.java
+++ src/test/java/cs02/model/video/VideoTest.java
@@ -27,6 +27,46 @@ class VideoTest {
 
     @Nested
     @DisplayName("add 기능 테스트")
+    class get {
+        @Test
+        @DisplayName("비디오 클립 목록에서 원하는 위치에 있는 클립을 찾을 수 있다.")
+        void success_get() {
+            var clip1 = clipBundle.clips()[0];
+            var clip2 = clipBundle.clips()[1];
+            var clip3 = clipBundle.clips()[2];
+
+            video.add(clip1);
+            video.add(clip2);
+            video.add(clip3);
+
+            var actualClip1 = video.get(0);
+            var actualClip2 = video.get(1);
+            var actualClip3 = video.get(2);
+
+            assertAll(
+                () -> assertEquals(clip1.id(), actualClip1.id()),
+                () -> assertEquals(clip2.id(), actualClip2.id()),
+                () -> assertEquals(clip3.id(), actualClip3.id())
+            );
+        }
+
+        @Test
+        @DisplayName("비디오 클립 목록에 존재하지 않는 클립을 찾으면 null을 반환한다.")
+        void fail_get() {
+            var clip1 = clipBundle.clips()[0];
+
+            video.add(clip1);
+
+            assertAll(
+                () -> assertNull(video.get(1)),
+                () -> assertNull(video.get(-1)),
+                () -> assertNull(video.get(100))
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("add 기능 테스트")
     class add {
         @Test
         @DisplayName("빈 비디오 클립 목록에 새 클립을 추가할 수 있다.")
